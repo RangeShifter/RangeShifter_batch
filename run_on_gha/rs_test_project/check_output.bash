c56d662116035b9d1360fe27c816e0cdc6947634
Theo Pannetier <theo@pannetier.nl>
2023-10-02 17:09:39 +0100

    compare RS output from check to pre-set files #9

--- run_on_gha/rs_test_project/check_output.bash
+++ run_on_gha/rs_test_project/check_output.bash
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+# Check that Rangeshifter outputs in Debug mode match pre-set expectations
+
+#TODO: check that seed in BatchLog.csv matches expectation, else don't bother with the rest
+#TODO: this script can probably be made more concise
+
+any_diff=0
+rm Outputs/DebugLog.txt # contains addresses so cannot match set expectation
+
 for filename in Outputs/*.txt; do
 if  [ $filename != "Outputs/anchor.txt" ]
 then
 	matching_expectation="../expected_output/${filename#Outputs/}"
-	echo $matching_expectation
+	if diff $filename $matching_expectation > tmp_diff.txt
+	then
+		do_nothing=1 # empty statement
+	else
+		echo "$filename differs from expectation:"
+		cat tmp_diff.txt
+		any_diff=1
+	fi
 fi
-
 done
 
-# Can I make the GHA job fail with this?
-exit 1
+if [ $any_diff -eq 1 ]
+then
+	exit 1 # check fails
+fi
+
