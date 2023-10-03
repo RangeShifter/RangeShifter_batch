268d4d6a5c40a03b20fd5b06f4fe9c9fda67ef5d
Theo Pannetier <theo@pannetier.nl>
2023-10-03 13:49:40 +0100

    Extract and compare RNG seed #9

--- run_on_gha/rs_test_project/check_output.bash
+++ run_on_gha/rs_test_project/check_output.bash
@@ -2,9 +2,18 @@
 
 # Check that Rangeshifter outputs in Debug mode match pre-set expectations
 
-any_diff=0
+# First check RNGs match, otherwise don't bother going further
+seed_this_run=$(grep -o "RANDOM SEED,[[:digit:]]*," Outputs/Batch1_RS_log.csv | grep -o [[:digit:]]*)
+seed_expected=$(grep -o "RANDOM SEED,[[:digit:]]*," ../expected_output/Batch1_RS_log.csv | grep -o [[:digit:]]*)
+if [ $seed_this_run -ne $seed_expected ]
+then
+	echo "RNG seed doesn't match: $seed_this_run vs expected $seed_expected"
+	exit 1 # check fails
+fi
 
-for filename in Outputs/[^\(DebugLog.txt\)\(git_anchor.txt\)]*; do # ignore DebugLog (bc if addresses) and anchor
+# Iteratively compare all output files with corresponding expectations
+any_diff=0
+for filename in Outputs/[^\(DebugLog.txt\)\(git_anchor.txt\)]*.txt; do # ignore DebugLog (bc if addresses) and anchor
 	matching_expectation="../expected_output/${filename#Outputs/}"
 	if ! diff $filename $matching_expectation > tmp_diff.txt
 	then
