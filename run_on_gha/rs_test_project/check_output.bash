535e3f14aac22636b467cf6b14d692c6bfd5782e
Theo Pannetier <theo@pannetier.nl>
2023-10-03 12:00:13 +0100

    more concise check_output

--- run_on_gha/rs_test_project/check_output.bash
+++ run_on_gha/rs_test_project/check_output.bash
@@ -2,28 +2,19 @@
 
 # Check that Rangeshifter outputs in Debug mode match pre-set expectations
 
-#TODO: this script can probably be made more concise
-
 any_diff=0
-rm Outputs/DebugLog.txt # contains addresses so cannot match set expectation
 
-for filename in Outputs/*; do
-if  [ $filename != "Outputs/anchor.txt" ]
-then
+for filename in Outputs/[^\(DebugLog.txt\)\(git_anchor.txt\)]*; do # ignore DebugLog (bc if addresses) and anchor
 	matching_expectation="../expected_output/${filename#Outputs/}"
-	if diff $filename $matching_expectation > tmp_diff.txt
+	if ! diff $filename $matching_expectation > tmp_diff.txt
 	then
-		do_nothing=1 # empty statement
-	else
 		echo "$filename differs from expectation:"
 		cat tmp_diff.txt
 		any_diff=1
 	fi
-fi
 done
 
 if [ $any_diff -eq 1 ]
 then
 	exit 1 # check fails
 fi
-
