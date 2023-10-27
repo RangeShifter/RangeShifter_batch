71ff979a48cff3d046f7327b0fd9a6fcfd4deaa1
Theo Pannetier <theo@pannetier.nl>
2023-10-27 16:13:33 +0100

    fix broken path

--- run_on_gha/rs_test_project/check_output.bash
+++ run_on_gha/rs_test_project/check_output.bash
@@ -2,17 +2,17 @@
 
 # Check that Rangeshifter outputs in Debug mode match pre-set expectations
 
+# MacOS number generation differs from Ubuntu and Windows so different set of expectations
+if [ $OSTYPE == "darwin21" ]; then osdir=macos ; else osdir=windows_ubuntu ; fi
+
 # First check RNGs match, otherwise don't bother going further
 seed_this_run=$(grep -o "RANDOM SEED,[[:digit:]]*," Outputs/Batch1_RS_log.csv | grep -o [[:digit:]]*)
-seed_expected=$(grep -o "RANDOM SEED,[[:digit:]]*," ../expected_output/Batch1_RS_log.csv | grep -o [[:digit:]]*)
+seed_expected=$(grep -o "RANDOM SEED,[[:digit:]]*," ../expected_output/${osdir}/Batch1_RS_log.csv | grep -o [[:digit:]]*)
 if [ $seed_this_run -ne $seed_expected ]
 then
 	echo "RNG seed doesn't match: $seed_this_run vs expected $seed_expected"
 	exit 1 # check fails
 fi
-
-# MacOS number generation differs from Ubuntu and Windows so different set of expectations
-if [ $OSTYPE == "darwin21" ]; then osdir=macos ; else osdir=windows_ubuntu ; fi
 
 # Iteratively compare all output files with corresponding expectations
 any_diff=0
