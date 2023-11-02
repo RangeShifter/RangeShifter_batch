60b35eac902779bdd0f12d9ded508364ec0c9bec
Theo Pannetier <theo@pannetier.nl>
2023-11-02 12:02:53 +0000

    we do need different expectations for macos

--- run_on_gha/rs_test_project/check_output.bash
+++ run_on_gha/rs_test_project/check_output.bash
@@ -2,6 +2,9 @@
 
 # Check that Rangeshifter outputs in Debug mode match pre-set expectations
 
+# MacOS number generation differs from Ubuntu and Windows so different set of expectations
+if [ $OSTYPE == "darwin21" ]; then osdir=macos ; else osdir=windows_ubuntu ; fi
+
 # First check RNGs match, otherwise don't bother going further
 seed_this_run=$(grep -o "RANDOM SEED,[[:digit:]]*," Outputs/Batch1_RS_log.csv | grep -o [[:digit:]]*)
 seed_expected=$(grep -o "RANDOM SEED,[[:digit:]]*," ../expected_output/${osdir}/Batch1_RS_log.csv | grep -o [[:digit:]]*)
@@ -17,7 +20,7 @@ for filename in Outputs/*.txt; do
 	# Ignore anchor; Batch and Debug logs are uninteresting to compare
 	if [ $filename != Outputs/git_anchor.txt ] && [ $filename != Outputs/BatchLog.txt ] && [ $filename != Outputs/DebugLog.txt ]
 	then 
-		matching_expectation="../expected_output/${filename#Outputs/}"
+		matching_expectation="../expected_output/${osdir}/${filename#Outputs/}"
 		# Ignore input filenames in Parameters which vary with OS
 		if ! diff $filename $matching_expectation  -I '^FILE NAME' > tmp_diff.txt
 		then
